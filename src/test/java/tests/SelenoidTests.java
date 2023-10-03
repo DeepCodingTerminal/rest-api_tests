810651e3752d1e3b267483b94228ad458a56d7de
Testorium <pmichael909@gmail.com>
2023-10-03 23:32:28 +0300

    1

---
+++ src/test/java/tests/SelenoidTests.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.get;
+
+public class SelenoidTests {
+    @Test
+    void checkTotal() {
+        get();
+
+    }
+}
