4c7bb46af590406d447d2250d40ab286e4410b70
Testorium <pmichael909@gmail.com>
2023-10-03 23:47:44 +0300

    2

--- src/test/java/tests/SelenoidTests.java
+++ src/test/java/tests/SelenoidTests.java
@@ -3,11 +3,14 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.get;
+import static org.hamcrest.Matchers.is;
 
 public class SelenoidTests {
     @Test
     void checkTotal() {
-        get();
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .body("total", is(20));
 
     }
 }
