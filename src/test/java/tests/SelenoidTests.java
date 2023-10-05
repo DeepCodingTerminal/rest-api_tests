a04f5d0746f72e535a43c57842a10d542d54311d
Testorium <pmichael909@gmail.com>
2023-10-06 00:43:50 +0300

    3

--- src/test/java/tests/SelenoidTests.java
+++ src/test/java/tests/SelenoidTests.java
@@ -3,6 +3,8 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.is;
 
 public class SelenoidTests {
@@ -11,6 +13,44 @@ public class SelenoidTests {
         get("https://selenoid.autotests.cloud/status")
                 .then()
                 .body("total", is(20));
-
+    }
+    @Test
+    void checkWithSomeLogsTotal() {
+        given()
+                .log().uri()
+//                .log().body()
+        .when()
+                .get("https://selenoid.autotests.cloud/status")
+        .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+    @Test
+    void checkChromeVersion() {
+        given()
+                .log().uri()
+//                .log().body()
+        .when()
+                .get("https://selenoid.autotests.cloud/status")
+        .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(20))
+                .body("browsers.chrome", hasKey("100.0"));
+    }
+    @Test
+    void checkResponseBadPractice() {
+        String response = given()
+                        .log().uri()
+        //                .log().body()
+                        .when()
+                        .get("https://selenoid.autotests.cloud/status")
+                        .then()
+                        .log().status()
+                        .log().body()
+                        .statusCode(200)
     }
 }
